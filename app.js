8772dec4ad5542a99aa00d0a0b3c931c9453a94e
chriswatia <chriswatia@gmail.com>
2019-09-27 03:13:41 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Recipe = require('./models/recipe');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://chris:GZTHrLkFYpgkMEBc@cluster0-blthw.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error(error);
+    });
+
+app.use(bodyParser.json());
+
+//Add headers to prevent CORS errors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+//Post route
+app.post('/api/recipes', (req, res, next) => {
+
+    const recipe = new Recipe({
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        difficulty: req.body.difficulty,
+        time: req.body.time
+    });
+    recipe.save().then(
+        () => {
+
+            res.status(201).json({
+                message: 'Recipe saved successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            console.log("Failed");
+            res.status(400).json({
+                error: error
+
+            });
+        }
+    );
+});
+
+
+
+//get single recipe
+app.get('/api/recipes/:id', (req, res, next) => {
+    Recipe.findOne({
+        _id: req.params.id
+    }).then(
+        (recipes) => {
+            res.status(200).json(recipes);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+});
+
+//Update a recipe
+app.put('/api/recipes/:id', (req, res, next) => {
+    const recipe = new Recipe({
+        _id: req.params.id,
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        difficulty: req.body.difficulty,
+        time: req.body.time
+    });
+    Recipe.updateOne({ _id: req.params.id }, recipe).then(
+        () => {
+            res.status(201).json({
+                message: 'Recipe updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+//delete a recipe
+app.delete('/api/recipes/:id', (req, res, next) => {
+    Recipe.deleteOne({ _id: req.params.id }).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+//Get route
+app.use('/api/recipes', (req, res, next) => {
+    Recipe.find().then(
+        (recipes) => {
+            res.status(200).json(recipes);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+module.exports = app;
